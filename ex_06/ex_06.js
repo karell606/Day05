1a564f0bed51324a1a04fcacd091f4074ad30f61
karell606 <karell.azabe@epitech.digital>
2024-10-03 17:24:29 +0200

    ex_06.js

---
+++ ex_06/ex_06.js
@@ -0,0 +1,9 @@
+document.getElementById('tag');
+ 
+window.onresize = function() {
+  alert("Size matters");
+};
+ 
+document.querySelector('img').onmouseover = function() {
+  alert("Pretty, isn't it?");
+};
